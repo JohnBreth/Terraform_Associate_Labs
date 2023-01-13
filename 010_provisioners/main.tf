92c2599efba22ecd558dbcdad46e43966ff9c580
johnbreth <jbreth1@gmail.com>
2023-01-13 10:58:42 -0500

    Updated main.tf with provider

--- 010_provisioners/main.tf
+++ 010_provisioners/main.tf
@@ -8,10 +8,15 @@ terraform {
       name = "provisioners"
     }
   }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.49.0"
     }
   }
+}
+
+provider "aws" {
+  region  = "us-east-1"
 }
