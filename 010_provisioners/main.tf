acbdfc720b327da8bc64f466a2f2a4a42962850b
johnbreth <jbreth1@gmail.com>
2023-01-13 10:57:02 -0500

    Updated main.tf with required providers infor for AWS

--- 010_provisioners/main.tf
+++ 010_provisioners/main.tf
@@ -1,9 +1,17 @@
+#Using AWS as a Provider, https://registry.terraform.io/providers/hashicorp/aws/0.1.0
 terraform {
+  #If no cloud section is used, this defaults to local. Organization must be created in terraform cloud.https://developer.hashicorp.com/terraform/tutorials/state/cloud-migrate
   cloud {
     organization = "CyberInsightLab"
 
     workspaces {
       name = "provisioners"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.49.0"
     }
   }
 }
